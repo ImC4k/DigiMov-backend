34a0128b40d00adb226437838ea6dd13937a9149
JackieLHT <jackie.leung@oocl.com>
2020-12-14 15:26:10 +0800

    [#5.BrowseCinema][Jackie] feat: CinemaIntegrationTest create and 404NotFound case

--- src/test/java/com/team_linne/digimov/integration/CinemaIntegrationTest.java
+++ src/test/java/com/team_linne/digimov/integration/CinemaIntegrationTest.java
@@ -61,7 +61,7 @@ public class CinemaIntegrationTest {
     }
 
     @Test
-    public void should_return_404_NotFound_when_getAll_given_invalid_cinema_id() throws Exception {
+    public void should_return_404NotFound_when_getAll_given_invalid_cinema_id() throws Exception {
         //when
         mockMvc.perform(MockMvcRequestBuilders.get("/cinemas/" + "5fc8913234ba53396c26a863"))
                 .andExpect(status().isNotFound());
@@ -88,5 +88,49 @@ public class CinemaIntegrationTest {
                 .andExpect(jsonPath("$.imageUrl").value("cinema1.jpg"))
                 .andExpect(jsonPath("$.openingHours").value("8:00-23:00"))
                 .andExpect(jsonPath("$.hotline").value("12345678"));
+    }
+
+    @Test
+    public void should_return_updated_cinema_when_update_given_valid_cinema_id_and_cinema_update_info() throws Exception {
+        Cinema cinema1 = new Cinema("cinema 1", "hong kong", "cinema1.jpg", "8:00-23:00", "12345678");
+        cinemaRepository.save(cinema1);
+        String cinemaAsJson = "{\n" +
+                "    \"name\": \"cinema 1 updated\",\n" +
+                "    \"address\": \"hong kong island\",\n" +
+                "    \"openingHours\": \"8:00-23:00\",\n" +
+                "    \"hotline\": \"12345678\",\n" +
+                "    \"imageUrl\": \"cinema1.jpg\"\n" +
+                "}";
+
+        //when
+        mockMvc.perform(MockMvcRequestBuilders.put("/cinemas/" + cinema1.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(cinemaAsJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").isString())
+                .andExpect(jsonPath("$.name").value("cinema 1 updated"))
+                .andExpect(jsonPath("$.address").value("hong kong island"))
+                .andExpect(jsonPath("$.imageUrl").value("cinema1.jpg"))
+                .andExpect(jsonPath("$.openingHours").value("8:00-23:00"))
+                .andExpect(jsonPath("$.hotline").value("12345678"));
+    }
+
+    @Test
+    public void should_return_404NotFound_when_update_given__invalid_cinema_id_and_cinema_update_info() throws Exception {
+        Cinema cinema1 = new Cinema("cinema 1", "hong kong", "cinema1.jpg", "8:00-23:00", "12345678");
+        cinemaRepository.save(cinema1);
+        String cinemaAsJson = "{\n" +
+                "    \"name\": \"cinema 1 updated\",\n" +
+                "    \"address\": \"hong kong island\",\n" +
+                "    \"openingHours\": \"8:00-23:00\",\n" +
+                "    \"hotline\": \"12345678\",\n" +
+                "    \"imageUrl\": \"cinema1.jpg\"\n" +
+                "}";
+
+        //when
+        mockMvc.perform(MockMvcRequestBuilders.put("/cinemas/" + "5fc8913234ba53396c26a863")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(cinemaAsJson))
+                .andExpect(status().isNotFound());
     }
 }
