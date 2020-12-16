1090ee2de53ae9cad1462b91fb75107411e9b5aa
Calvin Chu <calvinchu26@gmail.com>
2020-12-16 18:29:37 +0800

    [#8 cinema_session_picker_page][Calvin] update: MovieSessionResponse type

--- src/main/java/com/team_linne/digimov/dto/MovieSessionResponse.java
+++ src/main/java/com/team_linne/digimov/dto/MovieSessionResponse.java
@@ -1,7 +1,5 @@
 package com.team_linne.digimov.dto;
 
-import com.team_linne.digimov.model.House;
-import com.team_linne.digimov.model.Movie;
 import com.team_linne.digimov.model.SeatStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,13 +14,13 @@ import java.util.Map;
 @EqualsAndHashCode
 public class MovieSessionResponse {
     private String id;
-    private Movie movie;
-    private House house;
+    private MovieResponse movie;
+    private HouseResponse house;
     private Long startTime;
     private Map<String, Double> prices;
     private Map<Integer, SeatStatus> occupied;
 
-    public MovieSessionResponse(Movie movie, House house, Long startTime, Map<String, Double> prices, Map<Integer, SeatStatus> occupied) {
+    public MovieSessionResponse(MovieResponse movie, HouseResponse house, Long startTime, Map<String, Double> prices, Map<Integer, SeatStatus> occupied) {
         this.movie = movie;
         this.house = house;
         this.startTime = startTime;
