a020fbad071f381744ac9bc2a1b34a053f0cf664
Alex Liu <liual@oocl.com>
2020-12-16 13:39:23 +0800

    [#67.Cascading delete][Alex Liu] refactor: removed unused import

--- src/test/java/com/team_linne/digimov/integration/HouseIntegrationTest.java
+++ src/test/java/com/team_linne/digimov/integration/HouseIntegrationTest.java
@@ -13,8 +13,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.util.ArrayList;
-
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
